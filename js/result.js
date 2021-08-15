3759da186dff5bc254134fef9fc547d0754dfa56
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-08-16 02:48:54 +0900

    테스트용

--- js/result.js
+++ js/result.js
@@ -1,22 +1,30 @@
 const textMonitor = document.querySelector('.text-monitor');
 const analysisBox = document.querySelector('.suggestion-box');
 
-function init() {
-    let analysis = JSON.parse(localStorage.getItem('analysis'));
-    let originalText = JSON.parse(localStorage.getItem('original_text'));
+const resetBtn = document.querySelector('.reset-btn');
 
+const analysis = JSON.parse(localStorage.getItem('analysis'));
+const originalText = JSON.parse(localStorage.getItem('original_text'));
+
+const SELECTED_CLASSNAME = 'selected';
+
+function init() {
     textMonitor.innerText = originalText;
 
-    if(analysis.length === 0) {
+    if (analysis.length === 0) {
         // 분석 결과가 비었음을 알려주는 문구 추가
         return;
     }
 
-    for(let i = 0; i < analysis.length; ++i) {
+    for (let i = 0; i < analysis.length; ++i) {
         let now = analysis[i];
         const block = makeAnalysisBlock(now.start_idx, now.prior_str, now.new_str, now.removed);
+        block.id = `suggestion-${i}`;
+        block.addEventListener('click', handleClickSuggestion);
         analysisBox.appendChild(block);
     }
+
+    resetBtn.addEventListener('click', handleClickReset);
 }
 
 function makeAnalysisBlock(start_idx, prior_str, new_str, removed) {
@@ -31,7 +39,7 @@ function makeAnalysisBlock(start_idx, prior_str, new_str, removed) {
     ePriorStr.classList.add("sug-prior");
     eNewStr.classList.add("sug-new");
     eRemoved.classList.add("sug-removed");
-    
+
     eStartIdx.innerText = start_idx;
     ePriorStr.innerText = prior_str;
     eNewStr.innerText = new_str;
@@ -44,5 +52,41 @@ function makeAnalysisBlock(start_idx, prior_str, new_str, removed) {
 
     return eBlock;
 }
+
+
+function removeAnalysisBlock(idx) {
+    analysisBox.removeChild(document.querySelector(`.suggestion-box #suggestion-${idx}`));
+}
+
+
+function handleClickSuggestion(event) {
+    let block;
+
+    if (event.target.classList.contains('suggestion')) {
+        block = event.target;
+    } else {
+        block = event.target.parentElement;
+    }
+
+    if (block.classList.contains(SELECTED_CLASSNAME)) {
+        block.classList.remove(SELECTED_CLASSNAME);
+    } else {
+        block.classList.add(SELECTED_CLASSNAME);
+    }
+    //if(block.classList)
+}
+
+
+function handleClickReset() {
+    if(confirm("현재 변경사항을 모두 초기 상태로 되돌리시겠습니까?")) {
+        textMonitor.innerText = originalText;
+        const suggestions = analysisBox.querySelectorAll('.suggestion');
+
+        for(let i = 0; i < suggestions.length; ++i) {
+            suggestions[i].classList.remove(SELECTED_CLASSNAME);
+        }
+    }
+}
+
 
 init();
